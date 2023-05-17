5d2e0e058be945b7f528bc0617f8923ac7dd3d33
Chahat Kalsi <chahat.ck88@gmail.com>
2023-05-17 07:57:07 +0530

    fragment interpolation

--- opengl_5/source.cpp
+++ opengl_5/source.cpp
@@ -7,19 +7,20 @@ const int WIDTH = 800;
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout(location=0) in vec3 aPos;\n"
-"out vec4 vertexColor;\n"
+"layout(location=1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"vertexColor = vec4(0.2,0.7,0.4, 1.0);\n"
+"gl_Position = vec4(aPos, 1.0);\n"
+"ourColor = aColor;\n"
 "}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
-"in vec4 vertexColor;\n"
+"in vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"FragColor = vertexColor;\n"
+"FragColor = vec4(ourColor, 1.0);\n"
 "}\0";
 
 void framebuffer_resize_callback(GLFWwindow* window, int width, int height) {
@@ -105,9 +106,10 @@ int main() {
 	// triangle: VBOs, VAOs
 
 	float vertices[] = {
-		-0.5, -0.5, 0.0,
-		0.0, 0.5, 0.0,
-		0.5, -0.5, 0.0
+		// positions         // colors
+	 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+	-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+	 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top
 	};
 
 	// create
@@ -122,8 +124,12 @@ int main() {
 	// transfer
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float), (void*)0);
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 
 	// render loop
@@ -131,6 +137,13 @@ int main() {
 		processInput(window);
 
 		glUseProgram(shaderProgram);
+
+		// definition of uniform
+		float timeValue = glfwGetTime();
+		float greenValue = (std::sin(timeValue) / 2.0f) + 0.5f;
+		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
